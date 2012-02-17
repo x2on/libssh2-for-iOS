b33bac3821675b70fb42397e5bf088e09dc441cf
Felix Schulze <code@felixschulze.de>
2012-02-17 18:56:00 +0100

    Fix libgcrypt build error with iOS 5.0 SDK fixes 5

--- build-libgcrypt.sh
+++ build-libgcrypt.sh
@@ -22,7 +22,7 @@
 #  Change values here
 #
 VERSION="1.4.6"
-SDKVERSION="4.3"
+SDKVERSION="5.0"
 #
 ###########################################################################
 #
@@ -64,25 +64,29 @@ do
 	echo "Please stand by..."
 	tar zxf libgcrypt-${VERSION}.tar.gz -C src
 	cd src/libgcrypt-${VERSION}
+	
+	mkdir -p "${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk"
+	LOG="${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk/build-libgcrypt-${VERSION}.log"
+
+	echo "Patching libgcrypt to compile with iOS-SDK..."
+	echo "@see http://www.telesphoreo.org/browser/trunk/data/gcrypt/armasm.diff"
+	patch -p0 < ../../armasm.diff >> "${LOG}" 2>&1
+	echo "Patching done."
 
 	export DEVROOT="/Developer/Platforms/${PLATFORM}.platform/Developer"
 	export SDKROOT="${DEVROOT}/SDKs/${PLATFORM}${SDKVERSION}.sdk"
 	export CC=${DEVROOT}/usr/bin/gcc
 	export LD=${DEVROOT}/usr/bin/ld
-	export CPP=${DEVROOT}/usr/bin/cpp
+	export CPP=${DEVROOT}/usr/bin/llvm-cpp-4.2
 	export CXX=${DEVROOT}/usr/bin/g++
 	export AR=${DEVROOT}/usr/bin/ar
 	export AS=${DEVROOT}/usr/bin/as
 	export NM=${DEVROOT}/usr/bin/nm
-	export CXXCPP=$DEVROOT/usr/bin/cpp
+	export CXXCPP=$DEVROOT/usr/bin/llvm-cpp-4.2
 	export RANLIB=$DEVROOT/usr/bin/ranlib
 	export LDFLAGS="-arch ${ARCH} -pipe -no-cpp-precomp -isysroot ${SDKROOT} -L${CURRENTPATH}/lib"
 	export CFLAGS="-arch ${ARCH} -pipe -no-cpp-precomp -isysroot ${SDKROOT} -I${CURRENTPATH}/include"
 	export CXXFLAGS="-arch ${ARCH} -pipe -no-cpp-precomp -isysroot ${SDKROOT} -I${CURRENTPATH}/include"
-
-	mkdir -p "${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk"
-
-	LOG="${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk/build-libgcrypt-${VERSION}.log"
 
 	./configure --host=${ARCH}-apple-darwin --prefix="${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk" --disable-shared --enable-static --with-gpg-error-prefix="${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk" >> "${LOG}" 2>&1
 
